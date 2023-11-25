88da5407bc4b60d3c841636835294af51a2091ee
alab314 <almyr.abath@hotmail.com>
2023-11-25 08:39:39 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,69 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+# from fast_zero.app import app
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app)
+def test_root_deve_retornar_200_e_ola_mundo(client):
+    # client = TestClient(app)
 
     response = client.get('/')
 
     assert response.status_code == 200
     assert response.json() == {'messagem': 'Relou o mundo!'}
+
+
+def test_create_user(client):
+    # client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'saci',
+            'email': 'saci@sitiodoppa.com.br',
+            'password': 'carapuća',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'saci',
+        'email': 'saci@sitiodoppa.com.br',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'saci',
+                'email': 'saci@sitiodoppa.com.br',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'cuca',
+            'email': 'cuca@sitiodoppa.com.br',
+            'password': 'pirlimpimpim',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'cuca',
+        'email': 'cuca@sitiodoppa.com.br',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'Usuário apagado!'}
