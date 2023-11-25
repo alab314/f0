b3ed1fe076757f2378e155325b4789155cbbfe83
alab314 <almyr.abath@hotmail.com>
2023-11-25 13:48:55 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
-from fast_zero.security import get_password_hash
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
@@ -44,17 +50,22 @@ def read_users(
 
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(
-    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
 ):
-    db_user = session.scalar(select(User).where(User.id == user_id))
-    if db_user is None:
+
+    current_user = session.scalar(select(User).where(User.id == user_id))
+    if current_user is None:
         raise HTTPException(status_code=404, detail='Usuário não encontrado!')
-    db_user.username = user.username
-    db_user.password = user.password
-    db_user.email = user.email
+
+    current_user.username = user.username
+    current_user.password = get_password_hash(user.password)
+    current_user.email = user.email
     session.commit()
-    session.refresh(db_user)
-    return db_user
+    session.refresh(current_user)
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
@@ -65,3 +76,21 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
     session.delete(db_user)
     session.commit()
     return {'detail': 'Usuário apagado!'}
+
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+    if not user:
+        raise HTTPException(
+            status_code=400, detail='Senha ou email incorretos!'
+        )
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=400, detail='Senha ou email incorretos!'
+        )
+    access_token = create_access_token(data={'sub': user.email})
+    return {'access_token': access_token, 'token_type': 'bearer'}
